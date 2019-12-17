9db52bc9f30804101bfe3e0e3257d80b592687c3
rabindranathfv <rabindranathucv@gmail.com>
2019-12-17 16:06:32 -0300

    clean utils.js

--- src/server/server.js
+++ src/server/server.js
@@ -4,19 +4,14 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const setHeaders = require('./config/headers');
 
 // parse applicattion /x-www/form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 app.use(cors());
-app.use(setHeaders());
-
+app.use(require('./config/headers'));
 app.use(require('./api/routes/indexRoutes'));
-
-process.on('uncaughtException', handleFatalError)
-process.on('unhandledRejection', handleFatalError)
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err, resp) => {
     if (err) console.log(err)
