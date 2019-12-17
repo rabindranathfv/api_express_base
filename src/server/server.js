6cabf46e4eb8044d87ae8d12654b7d2efa49e374
rabindranathfv <rabindranathucv@gmail.com>
2019-12-17 15:57:05 -0300

    define headers into config folder

--- src/server/server.js
+++ src/server/server.js
@@ -4,14 +4,19 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const setHeaders = require('./config/headers');
 
 // parse applicattion /x-www/form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 app.use(cors());
+app.use(setHeaders());
 
 app.use(require('./api/routes/indexRoutes'));
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err, resp) => {
     if (err) console.log(err)
@@ -20,4 +25,4 @@ mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: tru
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando en el puerto ${process.env.PORT}`);
-})
+});
