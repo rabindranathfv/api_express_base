8a95192fb23768e4ebb5829403ae328b6162b542
rabindranathfv <rabindranathucv@gmail.com>
2019-12-23 16:48:09 -0300

    minor fix

--- src/server/server.js
+++ src/server/server.js
@@ -11,6 +11,7 @@ const Debug = require('debug');
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
+
 app.use(cors());
 app.use(require('./config/headers'));
 dotenv.config();
