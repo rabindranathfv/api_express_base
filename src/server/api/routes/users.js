9f71a451321727f71f6b32131e966de9b68f54d9
rabindranathfv <rabindranathucv@gmail.com>
2019-12-23 11:25:00 -0300

    delete some consoles

--- src/server/api/routes/users.js
+++ src/server/api/routes/users.js
@@ -4,27 +4,20 @@ const { checkToken, checkAdMinRole } = require('../middleware/auth');
 
 const app = express();
 
-/* checkToken */
 app.get('/users', checkToken, userCtrl.getUsers);
 
-/* checkToken, */
 app.get('/users/:id', checkToken, userCtrl.getUserById);
 
-/* [checkToken, checkAdMinRole] */
 app.post('/users', [checkToken, checkAdMinRole], userCtrl.postCreateUser);
 
-/* checkToken */
 app.put('/users/password', checkToken, userCtrl.updateUserPassword);
 
-/* [checkToken, checkAdMinRole], */
 app.put('/users/:id', [checkToken, checkAdMinRole], userCtrl.updateUser);
 
 /* hard delete */
-/* [checkToken, checkAdMinRole], */
 app.delete('/users/:id', [checkToken, checkAdMinRole], userCtrl.hardDeleteUser);
 
 /* soft delete */
-/* checkToken */
 app.delete('/users/2/:id', checkToken, userCtrl.softDeleteUser);
 
 module.exports = app;
